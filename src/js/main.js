8450ab8d0eae99bb3caf9ab17e008723910aa006
Ivanusa Vitaliy <sbvipok@gmail.com>
2016-10-31 17:36:14 +0200

    31.10.16 ve4er

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 
+  //Каруселька топ
+  $(".slick-top").slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    //autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1000
+  });
+
+  //Каруселька Новинки/Акции/Хиты продаж
+  $(".slick-bot").slick({
+    slidesToShow: 4,
+    dots: false,
+    infinite: true,
+    arrows: true,
+  })
 });
