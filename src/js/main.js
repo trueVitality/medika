fd293ce69b1237aaa9be32b7f9ef8f823db9aa54
Ivanusa Vitaliy <sbvipok@gmail.com>
2016-11-02 17:57:06 +0200

    02.11 ve4er

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
 
+  //Форма Заказать звонок
+  $(".phone-icons").click(function(event) {
+    event.preventDefault();
+    $('.popup-form').fadeIn(300,
+      function(){
+        $(this)
+          .css('display', 'block').animate({opacity: 1}, 300);
+      });
+  });
+  $(".phone-icon,.close_icon").click(function(event) {
+    event.preventDefault();
+    $(".popup-form").animate({opacity: 0}, 300, function(){
+      $(this).css('display', 'none');
+      $('.popup-form').fadeOut(350);
+    });
+  });
+
+  //кнопка вверх
+  var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  var delay = 800; // Задержка прокрутки
+    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+      if ($(this).scrollTop() > top_show) $('#up-button').fadeIn();
+      else $('#up-button').fadeOut();
+    });
+    $('#up-button').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+      /* Плавная прокрутка наверх */
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+
+  //ширина ховер контент блока на весь сайт
+  var widthR = $(window).width();
+  $("#content_drop").css('width', widthR);
+
   //Каруселька топ
   $(".slick-top").slick({
     dots: true,
     infinite: true,
     arrows: true,
-    //autoplay: true,
-    autoplaySpeed: 4000,
-    speed: 1000
+    //fade: true,
+    speed: 500
   });
 
   //Каруселька Новинки/Акции/Хиты продаж
