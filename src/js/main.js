b4abb911ddc3f83ee0784ff5a5c25e818b9e47f9
Ivanusa Vitaliy <sbvipok@gmail.com>
2016-11-28 19:16:43 +0200

    28.11 ve4er

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,68 @@
 $(document).ready(function() {
 
-  //slider
-  $('.nstSlider').nstSlider({
-    "rounding": {
-      "100": "1000",
-      "1000": "10000",
-      "10000": "10000"
-    },
-    "left_grip_selector": ".leftGrip",
-    "right_grip_selector": ".rightGrip",
-    "value_bar_selector": ".bar",
-    "value_changed_callback": function(cause, leftValue, rightValue) {
-      var $container = $(this).parent();
-      $container.find('.leftLabel').text(leftValue);
-      $container.find('.rightLabel').text(rightValue);
+  //slider jquery ui
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 25000,
+    values: [ 5000, 15000 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val(ui.values[ 0 ]);
+      $( "#amount2" ).val(ui.values[ 1 ]);
     }
+  });
+  $("#amount").val($("#slider-range").slider("values", 0 ));
+  $("#amount2").val($("#slider-range").slider( "values", 1 ));
+
+  // Изменение местоположения ползунка при вводиде данных в первый элемент input
+  $("input#amount").change(function(){
+    var value1=$("input#amount").val();
+    var value2=$("input#amount2").val();
+    if(parseInt(value1) > parseInt(value2)){
+      value1 = value2;
+      $("input#amount").val(value1);
+    }
+    $("#slider-range").slider("values",0,value1);
+  });
+
+  // Изменение местоположения ползунка при вводиде данных в второй элемент input
+  $("input#amount2").change(function(){
+    var value1=$("input#amount").val();
+    var value2=$("input#amount2").val();
+
+    if(parseInt(value1) > parseInt(value2)){
+      value2 = value1;
+      $("input#amount2").val(value2);
+    }
+    $("#slider-range").slider("values",1,value2);
+  });
+
+  // фильтрация ввода в поля
+  jQuery('#amount, #amount2').keypress(function(event){
+    var key, keyChar;
+    if(!event) var event = window.event;
+
+    if (event.keyCode) key = event.keyCode;
+    else if(event.which) key = event.which;
+
+    if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+    keyChar=String.fromCharCode(key);
+
+    if(!/\d/.test(keyChar))	return false;
+
+  });
+
+
+  //hover
+  $('.icon-list').hover(function() {
+    $(".icon-list img").attr('src', '../img/icons/cat/icon-list-hover.png');
+  }, function() {
+    $(".icon-list img").attr('src', '../img/icons/cat/icon-list.png');
+  });
+  $('.icon-block').hover(function() {
+    $(".icon-block img").attr('src', '../img/icons/cat/icon-block-hover.png');
+  }, function() {
+    $(".icon-block img").attr('src', '../img/icons/cat/icon-block.png');
   });
 
   //hover
@@ -84,6 +132,7 @@ $(document).ready(function() {
   //Каруселька Новинки/Акции/Хиты продаж
   $(".slick-bot").slick({
     slidesToShow: 4,
+    slidesToScroll: 2,
     dots: false,
     infinite: true,
     arrows: true,
